5e62048f9a56c65466b9d78ab03b599044569a5c
MoghiroiuBogdan <91489628+MoghiroiuBogdan@users.noreply.github.com>
2021-11-10 18:47:12 +0200

    lab 567

--- tests/testUndoRedo.py
+++ tests/testUndoRedo.py
@@ -1,4 +1,3 @@
-from Domain.creeazaLibrarie import getId
 from logic.CRUD import adauga_carte
 
 
@@ -40,11 +39,25 @@ def test_undo_redo():
         l = undoList.pop()
     assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
 
-    if len(redoList) > 0:
-        undoList.append(l)
-        l = redoList.pop()
-    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
-                    {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+    assert (l) == []
+
+    undoList.append(l)
+    redoList.clear()
+
+    l = adauga_carte("1", "moara cu noroc", "istoric", 100, "gold", l)
+
+    undoList.append(l)
+    redoList.clear()
+
+    l = adauga_carte("2", "moara cu noroc", "istoric", 100, "gold", l)
+
+    undoList.append(l)
+    redoList.clear()
+
+    l = adauga_carte("3", "moara cu noroc", "istoric", 100, "gold", l)
 
     if len(redoList) > 0:
         undoList.append(l)
@@ -53,3 +66,83 @@ def test_undo_redo():
                    {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
                    {'id': '3', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
 
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                    {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '3', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                    {'id': '2', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    undoList.append(l)
+    redoList.clear()
+    l=adauga_carte("4","moara cu noroc", "istoric", 100, "gold", l)
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '4', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(undoList) > 0:
+        redoList.append(l)
+        l = undoList.pop()
+
+    assert (l) == []
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '4', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
+
+    if len(redoList) > 0:
+        undoList.append(l)
+        l = redoList.pop()
+
+    assert (l) == [{'id': '1', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'},
+                   {'id': '4', 'titlu': 'moara cu noroc', 'gen': 'istoric', 'pret': 100, 'tipReducere': 'gold'}]
